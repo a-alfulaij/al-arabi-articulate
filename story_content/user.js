b5456844c9ffe3ba0eb5584fad95407564fed712
a-alfulaij <91006039+a-alfulaij@users.noreply.github.com>
2021-10-14 13:35:46 +0300

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6nw551Thqkc":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementsByTagName("iframe")[0].focus();
+console.log("done");
+}
+
